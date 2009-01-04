e3b8005ff6afa3e289e697e82bd8582dcd2724c0
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-01-04 13:47:40 +0000

    Checking in changes prior to tagging of version 0.02.  Changelog diff is:
    === Changes
    ==================================================================
    --- Changes	(revision 8664)
    +++ Changes	(local)
    @@ -1,4 +1,8 @@
     Revision history for Perl extension URI::Template::Restrict
     
    +0.02
    +        - no functional changes, no need to upgrade
    +        - fix POD (thanks to Tomohiro Teranishi)
    +
     0.01
             - original version
    Unknown target: CHANGES.
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/URI-Template-Restrict/trunk@27907 d0d07461-0603-4401-acd4-de1884942a52

--- lib/URI/Template/Restrict.pm
+++ lib/URI/Template/Restrict.pm
@@ -11,7 +11,7 @@ use URI::Escape qw(uri_escape_utf8);
 use URI::Template::Restrict::Expansion;
 use namespace::clean -except => ['meta'];
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 has 'template' => (
     is      => 'rw',
